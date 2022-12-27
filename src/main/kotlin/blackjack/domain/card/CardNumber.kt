5ee24bbb13f584c8ea9d6ea6b86ee4a6f681885b
shinaaaa <shin029abc@gmail.com>
2022-12-27 15:44:33 +0900

    refactor: CardNumber를 enum에서 sealed 클래스로 변경

--- src/main/kotlin/blackjack/domain/card/CardNumber.kt
+++ src/main/kotlin/blackjack/domain/card/CardNumber.kt
@@ -1,17 +1,33 @@
 package blackjack.domain.card
 
-enum class CardNumber(val text: String, vararg val value: Int) {
-    ACE("A", 1, 11),
-    TWO("2", 2),
-    THREE("3", 3),
-    FOUR("4", 4),
-    FIVE("5", 5),
-    SIX("6", 6),
-    SEVEN("7", 7),
-    EIGHT("8", 8),
-    NINE("9", 9),
-    TEEN("10", 10),
-    KING("K", 10),
-    QUEEN("Q", 10),
-    JACK("J", 10);
+import blackjack.domain.participant.vo.Score
+
+sealed class CardNumber(val text: String, val value: Score)
+
+class Ace : CardNumber("A", Score.of(1)) {
+    val aceValue = Score.of(11)
 }
+
+class Two : CardNumber("2", Score.of(2))
+
+class Three : CardNumber("3", Score.of(3))
+
+class Four : CardNumber("4", Score.of(4))
+
+class Five : CardNumber("5", Score.of(5))
+
+class Six : CardNumber("6", Score.of(6))
+
+class Seven : CardNumber("7", Score.of(7))
+
+class Eight : CardNumber("8", Score.of(8))
+
+class Nine : CardNumber("9", Score.of(9))
+
+class Ten : CardNumber("10", Score.of(10))
+
+class King : CardNumber("K", Score.of(10))
+
+class Queen : CardNumber("Q", Score.of(10))
+
+class Jack : CardNumber("J", Score.of(10))
